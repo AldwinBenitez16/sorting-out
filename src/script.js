b3d8be7d9b5bb51c5b7502aa9818c35465beaef6
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-02-27 16:53:18 -0500

    Adds file loader for images

--- src/script.js
+++ src/script.js
@@ -1,11 +1,18 @@
 import lodash from 'lodash';
 import './style.css';
+import Icon from './assets/images/image-1.png';
 
 function component() {
     const element = document.createElement('div');
     
     element.innerHTML = lodash.join(['Hello', 'Webpack'], ' ');
     element.classList.add('hello');
+
+    // add image to div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 };
