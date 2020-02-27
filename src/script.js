3f66ef53039c1325645033883ed0ab2eafd8e34c
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-02-27 16:44:38 -0500

    Adds styling to html using webpack

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
 import lodash from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
     
     element.innerHTML = lodash.join(['Hello', 'Webpack'], ' ');
+    element.classList.add('hello');
 
     return element;
 };
